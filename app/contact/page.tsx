c18dd5aa21c9f0c6648cdcddd642d2a9aec93a28
Matthew Lord <matty.j.lord@gmail.com>
2025-01-03 22:24:37 +1000

    style: improve style on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,14 @@
 import { ContactForm } from "@/components/forms/contact-form";
 export const metadata = {
     title: "Contact Us | GoSignal",
-    description: "Get in touch with GoSignal. We're here to help you with your digital marketing needs.",
+    description:
+        "Get in touch with GoSignal. We're here to help you with your digital marketing needs.",
 };
 
 export default function ContactPage() {
     return (
         <main className="flex w-full min-h-screen flex-col items-center justify-between">
-            <div className="w-full mx-auto px-4 md:px-6">               
-                <ContactForm  />
-            </div>
+            <ContactForm />
         </main>
     );
-} 
+}
