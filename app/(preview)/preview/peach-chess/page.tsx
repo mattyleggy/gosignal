be5f23498fae3c63f8c48371e771cd639d520422
Matthew Lord <matty.j.lord@gmail.com>
2025-01-28 13:59:21 +1000

    fix: use iframe of figma file instead of Image

--- app/(preview)/preview/peach-chess/page.tsx
+++ app/(preview)/preview/peach-chess/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
 export default function Page() {
     return (
-        <Image
-            src="/images/preview/peach-chess.png"
-            alt="Peach Chess"
-            width={2560}
-            height={1000}
-            style={{ width: "100%", height: "auto" }}
-        />
+        <iframe
+            src="https://embed.figma.com/proto/lpKh7bKKDxXtNWQPcglYrc/Chess---Ben-Peach?page-id=27%3A2&node-id=31-1933&viewport=-3265%2C414%2C0.67&scaling=min-zoom&content-scaling=fixed&embed-host=share"
+            style={{ width: "100vw", height: "100vh" }}
+            allowFullScreen
+        ></iframe>
     );
 }
