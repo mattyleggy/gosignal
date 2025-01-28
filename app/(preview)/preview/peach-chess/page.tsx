bb82ac7561e3d2a0730b7a3aadfbac153d54e28b
Matthew Lord <matty.j.lord@gmail.com>
2025-01-28 18:21:46 +1000

    feat: add in loading icon behind iframe

--- app/(preview)/preview/peach-chess/page.tsx
+++ app/(preview)/preview/peach-chess/page.tsx
@@ -1,9 +1,10 @@
+"use client";
+import PreviewFrame from "@/components/iframe/preview-frame";
+
 export default function Page() {
     return (
-        <iframe
-            src="https://embed.figma.com/proto/lpKh7bKKDxXtNWQPcglYrc/Chess---Ben-Peach?page-id=27%3A2&node-id=31-1933&viewport=-3265%2C414%2C0.67&scaling=min-zoom&content-scaling=fixed&embed-host=share"
-            style={{ width: "100vw", height: "100vh" }}
-            allowFullScreen
-        ></iframe>
+        <PreviewFrame 
+            iframeUrl="https://embed.figma.com/proto/lpKh7bKKDxXtNWQPcglYrc/Chess---Ben-Peach?page-id=27%3A2&node-id=31-1933&viewport=-3265%2C414%2C0.67&scaling=min-zoom&content-scaling=fixed&embed-host=share"
+        />
     );
 }
