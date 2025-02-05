8e1f68446d142aa907f9916d75a4645673b895e8
Matthew Lord <matty.j.lord@gmail.com>
2025-02-05 15:50:11 +1000

    content: update embed link for Peach Chess

--- app/(preview)/preview/peach-chess/page.tsx
+++ app/(preview)/preview/peach-chess/page.tsx
@@ -4,7 +4,7 @@ import PreviewFrame from "@/components/iframe/preview-frame";
 export default function Page() {
     return (
         <PreviewFrame 
-            iframeUrl="https://embed.figma.com/proto/lpKh7bKKDxXtNWQPcglYrc/Chess---Ben-Peach?page-id=27%3A2&node-id=31-1933&viewport=-3265%2C414%2C0.67&scaling=min-zoom&content-scaling=fixed&embed-host=share"
+            iframeUrl="https://embed.figma.com/proto/lpKh7bKKDxXtNWQPcglYrc/Peach-Chess?node-id=128-8&scaling=min-zoom&content-scaling=fixed&page-id=27%3A2&starting-point-node-id=128%3A8&embed-host=share"
         />
     );
 }
